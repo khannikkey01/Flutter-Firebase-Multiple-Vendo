15050255d1c165ec881294a3f45ef62a22c1739f
khannikkey01 <khannikkey@gmail.com>
2023-07-29 03:43:17 -0700

    RegisterScreen Completed With Welcom Text

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -7,64 +7,83 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Email',
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Paswwrod',
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width - 200,
-            child: OutlinedButton(
-              onPressed: () {
-                print('Login with Terminal');
-              },
-              child: const Text("Login"),
-            ),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Continue as Guest',
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome To Nadeem\'s App',
               style: TextStyle(
-                fontSize: 18,
-                letterSpacing: 5,
+                fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const RegisterScreen();
-                  },
-                ),
-              );
-            },
-            child: const Text(
-              "I haven't Account yet",
-              style: TextStyle(
-                fontSize: 16,
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
               ),
             ),
-          )
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Paswwrod',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 200,
+              child: OutlinedButton(
+                onPressed: () {
+                  print('Login with Terminal');
+                },
+                child: const Text("Login"),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Continue as Guest',
+                style: TextStyle(
+                  fontSize: 18,
+                  letterSpacing: 5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const RegisterScreen();
+                    },
+                  ),
+                );
+              },
+              child: const Text(
+                "I haven't Account yet",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/views/screens/auth/register_screen.dart
+++ lib/views/screens/auth/register_screen.dart
@@ -1,10 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:m_vendor_app/views/screens/auth/login_screen.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome To Nadeem\'s App',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Flexible(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'First Name',
+                      prefixIcon: Icon(
+                        Icons.person,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Flexible(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Last Name',
+                      prefixIcon: Icon(
+                        Icons.person,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                prefixIcon: Icon(
+                  Icons.email,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                prefixIcon: Icon(
+                  Icons.lock,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 200,
+              child: OutlinedButton(
+                onPressed: () {},
+                child: const Text('Sign UP'),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Continue as Geust',
+                style: TextStyle(
+                  fontSize: 18,
+                  letterSpacing: 5,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const LoginScreen();
+                }));
+              },
+              child: const Text(
+                'Already Have an Account',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
